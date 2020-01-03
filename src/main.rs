53ebae938552ad08ada6d05ff8cf3121e3bd73c7
r1cebank <siyuangao@gmail.com>
2020-01-03 00:27:37 -0800

    Adding cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("animan")
+        .version("0.1.0")
+        .author("Siyuan Gao <siyuangao@gmail.com>")
+        .arg(
+            Arg::with_name("dryrun")
+                .short("d")
+                .long("dryrun")
+                .help("Dry-run the rename operations"),
+        )
+        .arg(
+            Arg::with_name("name")
+                .short("n")
+                .long("name")
+                .takes_value(true)
+                .help("Override the series name"),
+        )
+        .get_matches();
+    println!("{:?}", matches)
 }
